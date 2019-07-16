6f67c757a6ea939edcd822a5461917d76ee65909
Awcmon <adcarbine@yahoo.com>
2019-07-16 14:56:30 -0700

    initial commit

---
+++ AlanDuongCom/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AlanDuongCom
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			foreach (string f in Directory.GetFiles("Data", "*.html"))
+			{
+				Console.WriteLine(f);
+			}
+		}
+	}
+}
