2f00484af4275388df5f39551f95a7b8217d56b4
Awcmon <adcarbine@yahoo.com>
2019-07-16 16:28:39 -0700

    working substitutions

--- AlanDuongCom/Program.cs
+++ AlanDuongCom/Program.cs
@@ -11,10 +11,24 @@ namespace AlanDuongCom
 	{
 		static void Main(string[] args)
 		{
+			string template = System.IO.File.ReadAllText(@"Data\basic.html");
+			string nav = System.IO.File.ReadAllText(@"Data\nav.html");
+			/*
 			foreach (string f in Directory.GetFiles("Data", "*.html"))
 			{
 				Console.WriteLine(f);
 			}
+			*/
+			string indexContent = System.IO.File.ReadAllText(@"Data\index.html");
+
+			string index = String.Copy(template);
+
+			index = index.Replace("$CONTENT$", indexContent);
+			index = index.Replace("$NAV$", nav);
+			index = index.Replace("$TITLE$", "Alan Duong");
+
+			System.IO.Directory.CreateDirectory("Out");
+			System.IO.File.WriteAllText(@"Out\index.html", index);
 		}
 	}
 }
