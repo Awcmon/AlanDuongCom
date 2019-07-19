c9901b5f5cd0478a1750a6b1a8b4746bc38f5dc2
Awcmon <adcarbine@gmail.com>
2019-07-19 00:10:53 -0700

    use number field for mil/moa conversion

--- AlanDuongCom/js/convertMilMoa.js
+++ AlanDuongCom/js/convertMilMoa.js
@@ -1,3 +1,4 @@
+//mil/moa conversion stuff
 var fMil;
 var fMoa;
 var fDecimals;
@@ -18,7 +19,88 @@ function updateFields() {
     fMoa.value = rawMoa.toFixed(parseInt(fDecimals.value));
     fMil.value = rawMil.toFixed(parseInt(fDecimals.value));
 }
+//ranging calculator stuff
+var fDistance;
+var fAngle;
+var fHeight;
+var btnMetric;
+var btnImperial;
+var radioSolveFor;
+var selDistanceUnit;
+var selAngleUnit;
+var selHeightUnit;
+var solveFor;
+function getRadioSelection(radio) {
+    for (var i = 0; i < radioSolveFor.length; i++) {
+        if (radio[i].checked) {
+            return radio[i].id;
+        }
+    }
+}
+function updateSolveFor() {
+    solveFor = getRadioSelection(radioSolveFor);
+    if (solveFor === "distance") {
+        fDistance.disabled = true;
+        fAngle.disabled = false;
+        fHeight.disabled = false;
+    }
+    else if (solveFor === "angle") {
+        fDistance.disabled = false;
+        fAngle.disabled = true;
+        fHeight.disabled = false;
+    }
+    else if (solveFor === "height") {
+        fDistance.disabled = false;
+        fAngle.disabled = false;
+        fHeight.disabled = true;
+    }
+}
+function presetMetric() {
+    selDistanceUnit.value = 'm';
+    selAngleUnit.value = 'MIL';
+    selHeightUnit.value = 'm';
+}
+function presetImperial() {
+    selDistanceUnit.value = 'yd';
+    selAngleUnit.value = 'MOA';
+    selHeightUnit.value = 'ft';
+}
+//unit, how many meters that unit is, or meters/unit
+//doing it the other way around actually results in numbers w/ longer decimals in the table
+var distConversions = [
+    ["mm", 0.001],
+    ["cm", 0.01],
+    ["m", 1.0],
+    ["km", 1000.0],
+    ["in", 0.0254],
+    ["ft", 0.3048],
+    ["yd", 0.9144],
+    ["mi", 1609.344]
+];
+function lengthUnitToMeter(length, unit) {
+    return length * distConversions[unit];
+}
+function lengthMeterToUnit(length, unit) {
+    return length / distConversions[unit];
+}
+function calculateFor() {
+    if (solveFor === "distance") {
+        fAngle.disabled = false;
+        fHeight.disabled = false;
+    }
+    else if (solveFor === "angle") {
+        fDistance.disabled = false;
+        fAngle.disabled = true;
+        fHeight.disabled = false;
+    }
+    else if (solveFor === "height") {
+        fDistance.disabled = false;
+        fAngle.disabled = false;
+        fHeight.disabled = true;
+    }
+}
 window.onload = function () {
+    //convert mil/moa stuff
     fMil = document.getElementById('fMil');
     fMoa = document.getElementById('fMoa');
     fDecimals = document.getElementById('fDecimals');
@@ -26,5 +108,21 @@ window.onload = function () {
     fMoa.addEventListener("change", function (e) { return calculateMil(); });
     fDecimals.addEventListener("change", function (e) { return updateFields(); });
     calculateMoa(); //initialize the MOA field
+    //ranger calculator stuff
+    fDistance = document.getElementById('fDistance');
+    fAngle = document.getElementById('fAngle');
+    fHeight = document.getElementById('fHeight');
+    btnMetric = document.getElementById('btnMetric');
+    btnImperial = document.getElementById('btnImperial');
+    radioSolveFor = document.getElementsByName('radioSolveFor');
+    selDistanceUnit = document.getElementById('selDistanceUnit');
+    selAngleUnit = document.getElementById('selAngleUnit');
+    selHeightUnit = document.getElementById('selHeightUnit');
+    btnMetric.addEventListener("click", function (e) { return presetMetric(); });
+    btnImperial.addEventListener("click", function (e) { return presetImperial(); });
+    for (var i = 0; i < radioSolveFor.length; i++) {
+        radioSolveFor[i].onchange = updateSolveFor;
+    }
+    updateSolveFor();
 };
 //# sourceMappingURL=convertMilMoa.js.map
--- js/convertMilMoa.ts
+++ js/convertMilMoa.ts
@@ -1,4 +1,5 @@
 ﻿
+//mil/moa conversion stuff
 let fMil : HTMLInputElement;
 let fMoa : HTMLInputElement;
 let fDecimals : HTMLInputElement;
@@ -25,7 +26,102 @@ function updateFields() {
 	fMil.value = rawMil.toFixed(parseInt(fDecimals.value));
 }
 
+//ranging calculator stuff
+let fDistance: HTMLInputElement;
+let fAngle: HTMLInputElement;
+let fHeight: HTMLInputElement;
+
+let btnMetric: HTMLButtonElement;
+let btnImperial: HTMLButtonElement;
+
+let radioSolveFor: NodeListOf<HTMLInputElement>;
+
+let selDistanceUnit: HTMLSelectElement;
+let selAngleUnit: HTMLSelectElement;
+let selHeightUnit: HTMLSelectElement;
+
+let solveFor: string;
+
+function getRadioSelection(radio: NodeListOf<HTMLInputElement>) : string
+{
+	for (let i = 0; i < radioSolveFor.length; i++) {
+		if (radio[i].checked) {
+			return radio[i].id;
+		}
+	}
+}
+
+function updateSolveFor() : any {
+	solveFor = getRadioSelection(radioSolveFor);
+	if (solveFor === "distance") {
+		fDistance.disabled = true;
+		fAngle.disabled = false;
+		fHeight.disabled = false;
+	}
+	else if (solveFor === "angle") {
+		fDistance.disabled = false;
+		fAngle.disabled = true;
+		fHeight.disabled = false;
+	}
+	else if (solveFor === "height") {
+		fDistance.disabled = false;
+		fAngle.disabled = false;
+		fHeight.disabled = true;
+	}
+}
+
+function presetMetric() {
+	selDistanceUnit.value = 'm';
+	selAngleUnit.value = 'MIL';
+	selHeightUnit.value = 'm';
+}
+
+function presetImperial() {
+	selDistanceUnit.value = 'yd';
+	selAngleUnit.value = 'MOA';
+	selHeightUnit.value = 'ft';
+}
+
+//unit, how many meters that unit is, or meters/unit
+//doing it the other way around actually results in numbers w/ longer decimals in the table
+let distConversions = [
+	["mm", 0.001],
+	["cm", 0.01],
+	["m", 1.0],
+	["km", 1000.0],
+	["in", 0.0254],
+	["ft", 0.3048],
+	["yd", 0.9144],
+	["mi", 1609.344]
+];
+
+function lengthUnitToMeter(length: number, unit: string): number {
+	return length * distConversions[unit];
+}
+
+function lengthMeterToUnit(length: number, unit: string): number {
+	return length / distConversions[unit];
+}
+
+function calculateFor() {
+	if (solveFor === "distance") {
+		fAngle.disabled = false;
+		fHeight.disabled = false;
+	}
+	else if (solveFor === "angle") {
+		fDistance.disabled = false;
+		fAngle.disabled = true;
+		fHeight.disabled = false;
+	}
+	else if (solveFor === "height") {
+		fDistance.disabled = false;
+		fAngle.disabled = false;
+		fHeight.disabled = true;
+	}
+}
+
 window.onload = () => {
+	//convert mil/moa stuff
 	fMil = <HTMLInputElement>document.getElementById('fMil');
 	fMoa = <HTMLInputElement>document.getElementById('fMoa');
 	fDecimals = <HTMLInputElement>document.getElementById('fDecimals');
@@ -35,4 +131,27 @@ window.onload = () => {
 	fDecimals.addEventListener("change", (e: Event) => updateFields());
 
 	calculateMoa(); //initialize the MOA field
+
+	//ranger calculator stuff
+	fDistance = <HTMLInputElement>document.getElementById('fDistance');
+	fAngle = <HTMLInputElement>document.getElementById('fAngle');
+	fHeight = <HTMLInputElement>document.getElementById('fHeight');
+
+	btnMetric = <HTMLButtonElement>document.getElementById('btnMetric');
+	btnImperial = <HTMLButtonElement>document.getElementById('btnImperial');
+
+	radioSolveFor = <NodeListOf<HTMLInputElement>>document.getElementsByName('radioSolveFor');
+
+	selDistanceUnit = <HTMLSelectElement>document.getElementById('selDistanceUnit');
+	selAngleUnit = <HTMLSelectElement>document.getElementById('selAngleUnit');
+	selHeightUnit = <HTMLSelectElement>document.getElementById('selHeightUnit');
+
+	btnMetric.addEventListener("click", (e: Event) => presetMetric());
+	btnImperial.addEventListener("click", (e: Event) => presetImperial());
+
+	for (let i = 0; i < radioSolveFor.length; i++) {
+		radioSolveFor[i].onchange = updateSolveFor; 
+	}
+
+	updateSolveFor();
 }
